113e0b3f67a5470e000c0e15d91d0c44ab18b66d
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2026-03-05 17:07:30 +0100

    feat(YouTube Music): Add support for `8.44.54`

--- patches/src/main/kotlin/app/morphe/patches/music/shared/Constants.kt
+++ patches/src/main/kotlin/app/morphe/patches/music/shared/Constants.kt
@@ -7,6 +7,7 @@ internal object Constants {
     val COMPATIBILITY_YOUTUBE_MUSIC: Pair<PackageName, Set<VersionName>> = Pair(
         "com.google.android.apps.youtube.music",
         setOf(
+            "8.44.54",
             "8.40.54",
             "8.10.52",
             "7.29.52",
